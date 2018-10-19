6b846c9695ee6b41a388e18a5ba10a97a2143c35
mbrenon <maxime.brenon@polytechnique.org>
2018-10-19 18:28:25 +0200

    Cleaning up.

--- snipsTools.py
+++ snipsTools.py
@@ -3,14 +3,22 @@ import io
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 
+
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
+
     def to_dict(self):
-        return {section: {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+        return {
+            section:
+                {option_name: option for option_name,
+                 option in self.items(section)}
+                for section in self.sections()
+        }
 
     @staticmethod
     def read_configuration_file(configuration_file):
         try:
-            with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            with io.open(configuration_file,
+                         encoding=CONFIGURATION_ENCODING_FORMAT) as f:
                 conf_parser = SnipsConfigParser()
                 conf_parser.readfp(f)
                 return conf_parser.to_dict()
@@ -31,4 +39,3 @@ class SnipsConfigParser(ConfigParser.SafeConfigParser):
         except (IOError, ConfigParser.Error) as e:
             print(e)
             return False
-
